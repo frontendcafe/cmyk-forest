409ba9ab27bb1c4fc91cd95160a1efd0cd1dd4be
facundo espinosa <facundo.espinosa@fing.edu.uy>
2020-12-09 19:58:15 -0300

    Configuring knex

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/cmyk_panel'
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
