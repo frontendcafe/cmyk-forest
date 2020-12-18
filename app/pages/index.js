2fe817ba53da378c4bb2a4ca48fb0dae0863c45a
vogric <blvogric@gmail.com>
2020-12-18 17:31:42 -0300

    Next.js and Tailwind CSS working

---
+++ app/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <>
+        <h1>Next.js and Tailwind working</h1>
+      </>
+    </div>
+  );
+}
