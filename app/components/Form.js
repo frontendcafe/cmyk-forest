7c9b42ecd10b2973ff4bb8a65cf5d2a1df926e7c
Nestor Acerbo <nestoracerbo@gmail.com>
2020-12-20 18:59:32 -0300

    Creado componente TextArea, modificado Dropdown para recibir clases, cambios en Form, esteticamente ya esta listo

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,16 +1,17 @@
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Dropdown from "./ui/Dropdown";
+import TextArea from "./ui/TextArea";
 
 const Form = () => {
   return (
     <>
       <section
-        class="p-7 ml-24 mt-5 flex flex-col items-center ml-8"
+        className="p-7 ml-24 mt-5 flex flex-col items-center ml-8"
         id="formcontainer"
       >
-        <img src="./assets/img/CMYK.png" alt="" class="w-72" />
-        <form class="grid grid-cols-2 gap-x-8 gap-y-4">
+        <img src="./assets/img/CMYK.png" alt="" className="w-72" />
+        <form className="grid grid-cols-2 gap-x-8 gap-y-4">
           <Input
             type="text"
             name="fullname"
@@ -31,58 +32,41 @@ const Form = () => {
               "Level 2 (Level 1 + React)",
             ]}
           />
-          <textarea
-            class="col-auto p-1 pl-3"
-            placeholder="Technologies"
-          ></textarea>
+          <TextArea placeholder="Technologies" className="col-auto p-1 pl-3" />
           <div>
-            <select class="col-auto p-1 pl-3 h-9" id="freetime">
-              <option value="" disabled selected>
-                {" "}
-                Free Time
-              </option>
-              <option>Value 1</option>
-              <option>Value 2</option>
-              <option>Value 3</option>
-              <option>Value 4</option>
-            </select>
-            <div class="flex place-content-between mt-3">
-              <label class="justify-self-start">Experience</label>
-              <input type="radio" value="y" class="" /> Yes
-              <input type="radio" value="n" class="" /> No
+            <Dropdown
+              id="freetime"
+              title="Free Time"
+              options={["Value 1", "Value 2", "Value 3", "Value 4"]}
+            />
+            <div className="flex place-content-between mt-3">
+              <label className="justify-self-start">Experience</label>
+              <Input type="radio" /> Yes
+              <Input type="radio" /> No
             </div>
           </div>
-          <input
-            type="text"
-            name="fullname"
-            id="fullname"
-            placeholder="Github"
-            class="col-auto p-1 pl-3"
-          />
-          <input
-            type="text"
-            name="fullname"
-            id="fullname"
+          <Input type="url" name="github" id="github" placeholder="Github" />
+          <Input
+            type="url"
+            name="linkedin"
+            id="linkedin"
             placeholder="LinkedIn"
-            class="col-auto p-1 pl-3"
           />
-          <input
+          <Input
             type="text"
-            name="fullname"
-            id="fullname"
+            name="discord"
+            id="discord"
             placeholder="Discord"
-            class="col-auto p-1 pl-3 h-9"
           />
-          <textarea
-            class="col-auto p-1 pl-3"
-            placeholder="Any questions? "
-          ></textarea>
-          <textarea
-            class="-mt-9 col-auto p-1 pl-3"
+          <TextArea
+            placeholder="Any questions?"
+            className="col-auto p-1 pl-3"
+          />
+          <TextArea
+            className="-mt-9 col-auto p-1 pl-3"
             placeholder="Tell us something about yourself"
-          ></textarea>
+          />
           <Button text="JOIN" />
-          {/* <button class="rounded-br-2xl text-white">JOIN</button> */}
         </form>
       </section>
     </>
