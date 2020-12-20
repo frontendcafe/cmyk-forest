354cd10a8e8c788bae629b0e98446d48382c85ff
vogric <blvogric@gmail.com>
2020-12-20 11:03:15 -0300

    FrontEnd Cafe Page - WIP

---
+++ app/pages/frontendcafe.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const FrontendCafe = () => {
+  return (
+    <Layout>
+      <h1>Frontend Café WIP</h1>
+    </Layout>
+  );
+};
+
+export default FrontendCafe;
