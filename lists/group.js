66bc5596564d3c385546690c4976805aeb949c4a
facundo espinosa <facundo.espinosa@fing.edu.uy>
2020-12-20 19:28:20 -0300

    add groups relationshps

--- lists/group.js
+++ lists/group.js
@@ -1,8 +1,20 @@
-const { Text } = require("@keystonejs/fields");
+const { Text, Relationship } = require("@keystonejs/fields");
 
 const Group = (keystone) => {
     return keystone.createList("group", {
         fields: {
+            lead: {
+                type: Relationship,
+                ref: 'user'
+            },
+            project: {
+                type: Relationship,
+                ref: 'project'
+            },
+            stuff_member: {
+                type: Relationship,
+                ref: 'stuff'
+            },
             name: {
                 type: Text,
             },
