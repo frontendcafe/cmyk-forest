188a8dc1aa3e5d99884add24906bc50717d654c6
Manuel <manuelejdelatorre@gmail.com>
2020-12-20 21:35:09 -0300

    Apollo usequery working, with an example on pages folder

--- app/lib/apolloClient.js
+++ app/lib/apolloClient.js
@@ -1,17 +1,14 @@
-import { useMemo } from "react";
 import { ApolloClient, HttpLink, InMemoryCache } from
     "@apollo/client";
 import fetch from 'node-fetch';
 
-let apolloClient;
 
-export default function createApolloClient() {
-    return new ApolloClient({
-        ssrMode: typeof window === "undefined", // set to true for SSR
-        link: new HttpLink({
-            uri: "http://localhost:3000",
-            fetch
-        }),
-        cache: new InMemoryCache(),
-    });
-}
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'http://localhost:3000/admin/api',
+        fetch
+    })
+});
+
+export default client;
