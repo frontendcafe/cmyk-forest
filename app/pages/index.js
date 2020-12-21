29d34e0036b0ef7500b69fe25058a7bc22ea9996
vogric <blvogric@gmail.com>
2020-12-21 12:54:38 -0300

    Yup: The following inputs were validated
    * fullname, email, technologies, github, linkedin, discord

--- app/pages/index.js
+++ app/pages/index.js
@@ -5,13 +5,13 @@ import Input from "../components/ui/Input";
 import Dropdown from "../components/ui/Dropdown";
 import TextArea from "../components/ui/TextArea";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function Home() {
   const formik = useFormik({
     initialValues: {
       fullname: "",
       email: "",
-      role: "",
       technologies: "",
       github: "",
       linkedin: "",
@@ -19,6 +19,16 @@ export default function Home() {
       questions: "",
       aboutYourself: "",
     },
+    validationSchema: Yup.object({
+      fullname: Yup.string().required("Full Name is required"),
+      email: Yup.string()
+        .required("Email is required")
+        .email("Email is not valid"),
+      technologies: Yup.string().required("Technologies is required"),
+      github: Yup.string().required("Your GitHub account is required"),
+      linkedin: Yup.string().required("Your LinkedIn account is required"),
+      discord: Yup.string().required("Your Discord account is required"),
+    }),
     onSubmit: (values) => {
       console.log(values);
     },
@@ -43,20 +53,33 @@ export default function Home() {
               placeholder="Full Name"
               value={formik.values.fullname}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.fullname && formik.errors.fullname && (
+              <div className="my-2 bg-red-100 border-l-2 border-red-500 text-red-700 p-2 mb-3">
+                <p className="font-bold text-sm "> {formik.errors.fullname} </p>
+              </div>
+            )}
             <input
               type="email"
               id="email"
               placeholder="E-mail adress"
               value={formik.values.email}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.email && formik.errors.email && (
+              <div className="my-2 bg-red-100 border-l-2 border-red-500 text-red-700 p-2 mb-3">
+                <p className="font-bold text-sm "> {formik.errors.email} </p>
+              </div>
+            )}
             <Dropdown
               title="Role"
               options={["Participant", "Leader"]}
               id="role"
               value={formik.values.role}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
             <Dropdown
               title="Level"
@@ -71,7 +94,15 @@ export default function Home() {
               id="technologies"
               value={formik.values.technologies}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.technologies && formik.errors.technologies && (
+              <div className="my-2 bg-red-100 border-l-2 border-red-500 text-red-700 p-2 mb-3">
+                <p className="font-bold text-sm ">
+                  {formik.errors.technologies}
+                </p>
+              </div>
+            )}
             <div>
               <Dropdown
                 id="freetime"
@@ -93,27 +124,46 @@ export default function Home() {
               placeholder="Github"
               value={formik.values.github}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.github && formik.errors.github && (
+              <div className="my-2 bg-red-100 border-l-2 border-red-500 text-red-700 p-2 mb-3">
+                <p className="font-bold text-sm "> {formik.errors.github} </p>
+              </div>
+            )}
             <input
               type="url"
               id="linkedin"
               placeholder="LinkedIn"
               value={formik.values.linkedin}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.linkedin && formik.errors.linkedin && (
+              <div className="my-2 bg-red-100 border-l-2 border-red-500 text-red-700 p-2 mb-3">
+                <p className="font-bold text-sm "> {formik.errors.linkedin} </p>
+              </div>
+            )}
             <input
               type="text"
               id="discord"
               placeholder="Discord"
               value={formik.values.discord}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.discord && formik.errors.discord && (
+              <div className="my-2 bg-red-100 border-l-2 border-red-500 text-red-700 p-2 mb-3">
+                <p className="font-bold text-sm "> {formik.errors.discord} </p>
+              </div>
+            )}
             <textarea
               placeholder="Any questions?"
               className="col-auto p-1 pl-3"
               id="questions"
               value={formik.values.questions}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
             <textarea
               className="-mt-9 col-auto p-1 pl-3"
@@ -121,6 +171,7 @@ export default function Home() {
               id="aboutYourself"
               value={formik.values.aboutYourself}
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
             <Button type="submit" text="JOIN" />
           </form>
