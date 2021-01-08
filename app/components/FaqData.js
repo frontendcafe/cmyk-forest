6cec577bc410e7b592d1c58447b48a054d49a156
vogric <blvogric@gmail.com>
2021-01-08 20:47:40 -0300

    Spinner was added in FaqData component

--- app/components/FaqData.js
+++ app/components/FaqData.js
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/react-hooks";
+import Spinner from "./ui/Spinner";
 
 const QUERY = gql`
   query allFaqs {
@@ -15,13 +16,16 @@ export const FaqData = () => {
   const { data, loading } = useQuery(QUERY);
   return (
     <div className="pl-20 mt-2 pr-20 ">
-      {data &&
+      {loading ? (
+        <Spinner />
+      ) : (
         data.allFaqs.map((faq) => (
           <>
             <h1 className="text-black text-xl mt-10">{faq.question}</h1>
             <p className="mt-3">{faq.answer}</p>
           </>
-        ))}
+        ))
+      )}
     </div>
   );
 };
